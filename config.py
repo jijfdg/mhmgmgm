26eebf6ae046feacc5e9f13412fe32b73060cf64
jijfdg <vokorobka@gmail.com>
2025-07-25 23:29:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
 import os
+from dotenv import load_dotenv
 
-BOT_TOKEN = os.getenv("8430063928:AAFidIFAxHZ9zxOpB6I6xHU99-Zt1YZ6zz4")
-ITPHONE_API_KEY = os.getenv("435520:AAtK4zW54f8Oueygnfm1vyHYLFat1CgFGsc")
-CRYPTOBOT_TOKEN = os.getenv("435520:AAtK4zW54f8Oueygnfm1vyHYLFat1CgFGsc")
+load_dotenv()
 
+def get_env_var(name: str) -> str:
+    value = os.getenv(name)
+    if not value:
+        raise ValueError(f"[❌] Переменная окружения '{name}' не задана!")
+    return value
+
+BOT_TOKEN = get_env_var("8430063928:AAFidIFAxHZ9zxOpB6I6xHU99-Zt1YZ6zz4")
+ITPHONE_API_KEY = get_env_var("435520:AAtK4zW54f8Oueygnfm1vyHYLFat1CgFGsc")
+CRYPTOBOT_TOKEN = get_env_var("435520:AAtK4zW54f8Oueygnfm1vyHYLFat1CgFGsc")
+
+# API URLs
 ITPHONE_API_URL = "https://api.itphone.app"
 CRYPTOBOT_API_URL = "https://pay.crypt.bot/api"
