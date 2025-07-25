d064546628b31d3f8efd4d93f0bde13c6cb7464d
jijfdg <vokorobka@gmail.com>
2025-07-25 23:49:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-print("[DEBUG] BOT_TOKEN:", os.getenv("BOT_TOKEN"))
 import os
 from dotenv import load_dotenv
 
@@ -6,6 +5,7 @@ load_dotenv()
 
 def get_env_var(name: str) -> str:
     value = os.getenv(name)
+    print(f"[DEBUG] {name} =", value)  # Выводим переменные окружения
     if not value:
         raise ValueError(f"[❌] Переменная окружения '{name}' не задана!")
     return value
@@ -14,6 +14,5 @@ BOT_TOKEN = get_env_var("BOT_TOKEN")
 ITPHONE_API_KEY = get_env_var("ITPHONE_API_KEY")
 CRYPTOBOT_TOKEN = get_env_var("CRYPTOBOT_TOKEN")
 
-# API URLs
 ITPHONE_API_URL = "https://api.itphone.app"
 CRYPTOBOT_API_URL = "https://pay.crypt.bot/api"
