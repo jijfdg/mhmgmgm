ede4bb0be9bd7fb2922752f545db57b782646bf6
jijfdg <vokorobka@gmail.com>
2025-07-25 23:17:57 +0300

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ITPHONE_API_KEY = os.getenv("ITPHONE_API_KEY")
-CRYPTOBOT_TOKEN = os.getenv("CRYPTOBOT_TOKEN")
+BOT_TOKEN = os.getenv("8430063928:AAFidIFAxHZ9zxOpB6I6xHU99-Zt1YZ6zz4")
+ITPHONE_API_KEY = os.getenv("435520:AAtK4zW54f8Oueygnfm1vyHYLFat1CgFGsc")
+CRYPTOBOT_TOKEN = os.getenv("435520:AAtK4zW54f8Oueygnfm1vyHYLFat1CgFGsc")
 
 ITPHONE_API_URL = "https://api.itphone.app"
 CRYPTOBOT_API_URL = "https://pay.crypt.bot/api"
